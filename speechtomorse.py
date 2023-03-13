d5ab6e605edf68e9ca39599af83c1aa5fe9487e8
ananthapadmanabhanv <85188547+ananthapadmanabhanv@users.noreply.github.com>
2023-03-13 18:42:59 +0530

    Create speechtomorse.py

---
+++ speechtomorse.py
@@ -0,0 +1,82 @@
+import wave
+import speech_recognition as sr
+import pyaudio
+
+FRAMES_PER_BUFFER = 3200
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+
+p = pyaudio.PyAudio()
+
+stream = p.open(
+    format=FORMAT,
+    channels=CHANNELS,
+    rate=RATE,
+    input=True,
+    frames_per_buffer=FRAMES_PER_BUFFER
+)
+
+print("start recording")
+
+seconds = 5
+frames = []
+for i in range(0, int(RATE/FRAMES_PER_BUFFER*seconds)):
+    data = stream.read(FRAMES_PER_BUFFER)
+    frames.append(data)
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+obj = wave.open("output.wav", "wb")
+obj.setnchannels(CHANNELS)
+obj.setsampwidth(p.get_sample_size(FORMAT))
+obj.setframerate(RATE)
+obj.writeframes(b"".join(frames))
+obj.close()
+
+filename = "output.wav"
+r = sr.Recognizer()
+
+with sr.AudioFile(filename) as source:
+    audio_data = r.record(source)
+    text = r.recognize_google(audio_data)
+
+#print(text)
+
+
+MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+                   'C': '-.-.', 'D': '-..', 'E': '.',
+                   'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-',
+                   'L': '.-..', 'M': '--', 'N': '-.',
+                   'O': '---', 'P': '.--.', 'Q': '--.-',
+                   'R': '.-.', 'S': '...', 'T': '-',
+                   'U': '..-', 'V': '...-', 'W': '.--',
+                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                   '1': '.----', '2': '..---', '3': '...--',
+                   '4': '....-', '5': '.....', '6': '-....',
+                   '7': '--...', '8': '---..', '9': '----.',
+                   '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                   '?': '..--..', '/': '-..-.', '-': '-....-',
+                   '(': '-.--.', ')': '-.--.-'}
+
+
+def encrypt(message):
+    cipher = ''
+    for letter in message:
+        if letter != ' ':
+
+            cipher += MORSE_CODE_DICT[letter] + ' '
+        else:
+            cipher += ' '
+    return cipher
+
+print('Input Voice:'+'\t'+text)
+message = text
+result = encrypt(message.upper())
+print('Morse Code:'+'\t'+result)
+
+
+
